2f9075e48ce6b1399c572bdf5cf4e6bf703e6f63
abumosameh <abumosam@sheridancollege.ca>
2026-03-23 22:24:57 -0400

    fix sed deploy.sh

--- Ops/deploy-grocersave.sh
+++ Ops/deploy-grocersave.sh
@@ -133,7 +133,7 @@ check_k8s() {
 
     echo -e "${YELLOW}Configuring kubectl...${NC}"
     awslocal eks update-kubeconfig --name "$CLUSTER_NAME"
-    sed -i 's/command: aws/command: awslocal/g' ~/.kube/config	
+    #sed -i 's/command: aws/command: awslocal/g' ~/.kube/config
 
     if ! kubectl get nodes > /dev/null 2>&1; then
          echo -e "${RED}Failed to connect to K8s.${NC}"
